91c8829fb686a60e6b7da4d0093e01d019a77a21
Tomas Silva Queiroga <ra137748@lima.lab.ic.unicamp.br>
2014-06-18 21:17:54 -0300

    build dos arquivos

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
 source /home/specg12-2/mc404/simulador/set_path.sh
 
-rm -rf run
-mkdir run
-cd run
-
-arm-eabi-as -g faz_nada.s -o faz_nada.o
-arm-eabi-ld faz_nada.o -o faz_nada -g --section-start=.iv=0x778005e0 -Ttext=0x77800700 -Tdata=0x77801900 -e 0x778005e0
+make clean
+make
 
 arm-eabi-as -g ra137748.s -o ra137748.o
 arm-eabi-ld ra137748.o -o ra137748 -g --section-start=.iv=0x778005e0 -Ttext=0x77800700 -Tdata=0x77801800 -e 0x778005e0
 mksd.sh --so ra137748 --user faz_nada
 arm-sim --rom=/home/specg12-1/mc404/simulador/dumboot.bin --sd=disk.img -g
-cd .. 
+
