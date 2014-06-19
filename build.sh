3948f9012a8c6a8699cc78f331c3486db106342f
TomasSQ <tomasqueiroga@gmail.com>
2014-06-19 18:56:59 -0300

    possivl rodar em outra porta

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ source /home/specg12-2/mc404/simulador/set_path.sh
 make clean
 make
 
-arm-eabi-as -g boot.s -o boot.o && arm-eabi-ld boot.o -o boot -g --section-start=.iv=0x778005e0 -Ttext=0x77800700 -Tdata=0x77801800 -e 0x778005e0 && mksd.sh --so boot --user dummy_user && arm-sim --rom=/home/specg12-1/mc404/simulador/dumboot.bin --sd=disk.img -g
+arm-eabi-as -g boot.s -o boot.o && arm-eabi-ld boot.o -o boot -g --section-start=.iv=0x778005e0 -Ttext=0x77800700 -Tdata=0x77801800 -e 0x778005e0 && mksd.sh --so boot --user dummy_user && arm-sim --rom=/home/specg12-1/mc404/simulador/dumboot.bin --sd=disk.img -g $1
