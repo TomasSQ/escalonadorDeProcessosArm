da41fd035e71191062f6bfb8582e322374ec4f47
Diego Rocha <diego.rocha.comp@gmail.com>
2014-06-19 21:26:58 -0300

    Alinhamento dos comentários

--- boot.s
+++ boot.s
@@ -38,15 +38,15 @@ SET_GPT:
 
 SET_UART:
 	.set UART_BASE,			0x53FBC000
-	.set UART_UTXD,			0x0040						@ 0x53FBC040
-	.set UART_UCR1,			0x0080						@ 0x53FBC080
-	.set UART_UCR2,			0x0084						@ 0x53FBC084
-	.set UART_UCR3,			0x0088						@ 0x53FBC088
-	.set UART_UCR4,			0x008C						@ 0x53FBC08C
-	.set UART_UFCR,			0x0090						@ 0x53FBC090
-	.set UART_USR1,			0x0094						@ 0x53FBC094
-	.set UART_UBIR,			0x00A4						@ 0x53FBC0A4
-	.set UART_UBMR,			0x00A8						@ 0x53FBC0A8
+	.set UART_UTXD,			0x0040				@ 0x53FBC040
+	.set UART_UCR1,			0x0080				@ 0x53FBC080
+	.set UART_UCR2,			0x0084				@ 0x53FBC084
+	.set UART_UCR3,			0x0088				@ 0x53FBC088
+	.set UART_UCR4,			0x008C				@ 0x53FBC08C
+	.set UART_UFCR,			0x0090				@ 0x53FBC090
+	.set UART_USR1,			0x0094				@ 0x53FBC094
+	.set UART_UBIR,			0x00A4				@ 0x53FBC0A4
+	.set UART_UBMR,			0x00A8				@ 0x53FBC0A8
 
 .org 0x100
 .text
@@ -54,26 +54,26 @@ RESET_HANDLER:
 	ldr		r0, =interrupt_vector
 	mcr		p15, 0, r0, c12, c0, 0
 
-	ldr		sp, =SVC_STACK					@ Configura stack do supervisor
+	ldr		sp, =SVC_STACK						@ Configura stack do supervisor
 
-	msr		CPSR_c, #0xDF					@ Entra em modo system, FIQ/IRQ desabilitados
+	msr		CPSR_c, #0xDF						@ Entra em modo system, FIQ/IRQ desabilitados
 	ldr		sp, =USR_STACK
 
-	msr		CPSR_c, #0xD1					@ Entra em modo FIQ, FIQ/IRQ desabilitados
+	msr		CPSR_c, #0xD1						@ Entra em modo FIQ, FIQ/IRQ desabilitados
 	ldr		sp, =FIQ_STACK
 
-	msr		CPSR_c, #0xD2					@ Entra em modo IRQ, FIQ/IRQ desabilitados
+	msr		CPSR_c, #0xD2						@ Entra em modo IRQ, FIQ/IRQ desabilitados
 	ldr		sp, =IRQ_STACK
 
-	msr		CPSR_c, #0xD7					@ Entra em modo abort, FIQ/IRQ desabilitados
+	msr		CPSR_c, #0xD7						@ Entra em modo abort, FIQ/IRQ desabilitados
 	ldr		sp, =ABT_STACK
 
-	msr		CPSR_c, #0xDB					@ Entra em modo undefined, FIQ/IRQ desabilitados
+	msr		CPSR_c, #0xDB						@ Entra em modo undefined, FIQ/IRQ desabilitados
 	ldr		sp, =UND_STACK
 
-	msr		CPSR_c, #0xD3					@ Volta ao modo supervisor, FIQ/IRQ desabilitados
+	msr		CPSR_c, #0xD3						@ Volta ao modo supervisor, FIQ/IRQ desabilitados
 
-											@ Liga o GPT
+												@ Liga o GPT
 	ldr		r0, =GPT_BASE
 
 	mov		r1, #0x41
@@ -87,27 +87,27 @@ RESET_HANDLER:
 
 	mov		r1, #1
 	str		r1, [r0, #GPT_IR]
-											@ Liga o controlador de interrupcoes
-											@ R1 <= TZIC_BASE
+												@ Liga o controlador de interrupcoes
+												@ R1 <= TZIC_BASE
 	ldr		r1, =TZIC_BASE
-											@ Configura interrupcao 39 do GPT como nao segura
+												@ Configura interrupcao 39 do GPT como nao segura
 	mov		r0, #(1 << 7)
 	str		r0, [r1, #TZIC_INTSEC1]
-											@ Habilita interrupcao 39 (GPT)
-											@ reg1 bit 7 (gpt)
+												@ Habilita interrupcao 39 (GPT)
+												@ reg1 bit 7 (gpt)
 	mov		r0, #(1 << 7)
 	str		r0, [r1, #TZIC_ENSET1]
-											@ Configure interrupt39 priority as 1
-											@ reg9, byte 3
+												@ Configure interrupt39 priority as 1
+												@ reg9, byte 3
 	ldr		r0, [r1, #TZIC_PRIORITY9]
 	bic		r0, r0, #0xFF000000
 	mov		r2, #1
 	orr		r0, r0, r2, lsl #24
 	str		r0, [r1, #TZIC_PRIORITY9]
-											@ Configure PRIOMASK as 0
+												@ Configure PRIOMASK as 0
 	eor		r0, r0, r0
 	str		r0, [r1, #TZIC_PRIOMASK]
-											@ Habilita o controlador de interrupcoes
+												@ Habilita o controlador de interrupcoes
 	mov		r0, #1
 	str		r0, [r1, #TZIC_INTCTRL]
 
@@ -145,10 +145,10 @@ RESET_HANDLER:
 	add		r1, r1, #0x34
 	str		r1, [r0, #UART_UBMR]
 
-											@ Carrega endereço do início do programa de usuário
+												@ Carrega endereço do início do programa de usuário
 	ldr		r0, =USER_TEXT
 	ldr		r0, [r0]
-											@ Entra em modo de usuário com interrupções habilitadas
+												@ Entra em modo de usuário com interrupções habilitadas
 	msr		CPSR_c, #0x10
 	mov		PC, r0
 
@@ -170,11 +170,11 @@ SVC_END:
 	mov		pc, lr
 
 IRQ_HANDLER:
-	ldr		r0, =GPT_BASE					@ zera flag, para nao tratar esta interrupcao novamente
+	ldr		r0, =GPT_BASE						@ zera flag, para nao tratar esta interrupcao novamente
 	mov		r1, #1
 	str		r1, [r0, #GPT_SR]
 
-	sub		lr, lr, #4						@ retorna
+	sub		lr, lr, #4							@ retorna
 	movs		pc, lr
 
 SYSCALL_EXIT:
