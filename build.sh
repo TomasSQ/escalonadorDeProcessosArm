5af236f57b892adf83eb3ee81aa9b0bcac3c0f65
TomasSQ <tomasqueiroga@gmail.com>
2014-06-25 20:11:41 -0300

    submissao

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-source /home/specg12-2/mc404/simulador/set_path.sh
+source /home/tomas/Unicamp/MC404/simulador/set_path.sh
 
 make clean
 make
 
-arm-eabi-as -g boot.s -o boot.o && arm-eabi-ld boot.o -o boot -g --section-start=.iv=0x778005e0 -Ttext=0x77800700 -Tdata=0x77801800 -e 0x778005e0 && mksd.sh --so boot --user dummy_user && arm-sim --rom=/home/specg12-1/mc404/simulador/dumboot.bin --sd=disk.img -g $1
+arm-eabi-as -g boot.s -o boot.o && arm-eabi-ld boot.o -o boot -g --section-start=.iv=0x778005e0 -Ttext=0x77800700 -Tdata=0x77801800 -e 0x778005e0 && mksd.sh --so boot --user dummy_user && arm-sim --rom=/home/tomas/Unicamp/MC404/simulador/dumboot.bin --sd=disk.img -g $1
